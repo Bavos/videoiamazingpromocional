d719c7ae16581eb69a3c8ca82c3fd2ca5b5251eb
Bavos <87132712+Bavos@users.noreply.github.com>
2026-04-24 14:12:07 -0300

    Remove custom Remotion configuration settings

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,9 +1,1 @@
-import { Config } from "@remotion/cli/config";
 
-Config.overrideWebpackConfig((currentConfiguration) => {
-  return currentConfiguration;
-});
-
-Config.setBrowserExecutable(null);
-Config.setChromiumDisableWebSecurity(false);
-Config.setConcurrency(4);
